f24ac32f2bd1dcb49535e0508f8701bf9606a6c3
Churikov Daniil <ddosia@gmail.com>
2013-12-26 19:51:11 +0400

    Add unit tests.

--- src/mekao_utils.erl
+++ src/mekao_utils.erl
@@ -42,3 +42,38 @@ intersperse4({[I1 | I1s], [I2 | I2s], [I3 | I3s], [I4 | I4s]}, Sep, ValFun) ->
 %%% Internal functions
 %%%===================================================================
 
+%%%===================================================================
+%%% Tests
+%%%===================================================================
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+intersperse_test_() ->
+    L = [a, b, c],
+    ValFun = fun (a) -> 1; (b) -> 2; (c) -> 3 end,
+
+    [
+        ?_assert([1, x, 2, x, 3] == intersperse(L, x, ValFun)),
+        ?_assert([1] == intersperse([a], x, ValFun)),
+        ?_assert([] == intersperse([], x, ValFun))
+    ].
+
+intersperse4_test_() ->
+    L4 = {[a, b, c, d], [b, c, d, a], [c, d, a, b], [d, a, b, c]},
+
+    ValFun =
+        fun
+            ({a, _, _, _}) -> 1;
+            ({b, _, _, _}) -> 2;
+            ({c, _, _, _}) -> 3;
+            ({d, _, _, _}) -> 4
+        end,
+
+    [
+        ?_assert([1, x, 2, x, 3, x, 4] == intersperse4(L4, x, ValFun)),
+        ?_assert([1] == intersperse4({[a], [b], [c], [d]}, x, ValFun)),
+        ?_assert([] == intersperse4({[], [], [], []}, x, ValFun))
+    ].
+
+-endif.
